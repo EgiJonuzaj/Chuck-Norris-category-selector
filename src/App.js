8f3df5d94cef95d8a6a0377f220c2f9bfeb33256
egi jonuzaj <jonuzaj2000@gmail.com>
2021-11-19 20:48:51 +0100

    Setup redux store and test the api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <></>;
+  const [categories, setCategories] = useState([""]);
+
+  const fetchData = async () => {
+    const result = await axios.get(
+      "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/categories",
+      {
+        headers: {
+          accept: "application/json",
+          "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "479ecfdeacmsh5c6f8b141455f51p109312jsnb6decb0da1e7",
+        },
+      }
+    );
+    setCategories(result.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {categories.map((e) => {
+        return <h3>{e}</h3>;
+      })}
+    </>
+  );
 }
 
 export default App;
